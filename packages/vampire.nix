2c6421704fb27372a7d28446cfdb2cffdb26aeca
puyral <7871851+puyral@users.noreply.github.com>
2024-04-05 09:13:41 +0200

    maybe better vampire

--- packages/vampire.nix
+++ packages/vampire.nix
@@ -1,15 +1,31 @@
 { pkgs, ... }:
 with pkgs;
-{
-  packages.vampire = vampire.overrideAttrs (finalAttrs: previousAttrs: {
-          src = pkgs.fetchFromGitHub {
-            owner = "vprover";
-            repo = "vampire";
-            rev = "23d7e9c3b61479bc14e1687f917c22f80dc7c8f1";
-            sha256 = "sha256-Aig86q+f+XvQCqJyHqR610+4+mzy+gwAxY6lSdRAvss=";
-          };
-          version = "master";
-          doCheck = false;
-          patches = [];
-        });
+let
+  mkvampire = version: src:
+    vampire.overrideAttrs (finalAttrs: previousAttrs: {
+      inherit version src;
+      doCheck = false;
+      patches = [ ];
+    });
+
+in {
+  packages = rec {
+    vampire = vampire-master;
+
+    vampire-master = mkvampire "master" (fetchFromGitHub {
+      owner = "vprover";
+      repo = "vampire";
+      rev = "23d7e9c3b61479bc14e1687f917c22f80dc7c8f1";
+      hash = "sha256-Aig86q+f+XvQCqJyHqR610+4+mzy+gwAxY6lSdRAvss=";
+    });
+
+    vampire-ccsa = mkvampire "ccsa" (fetchFromGitHub {
+      owner = "vprover";
+      repo = "vampire";
+      rev = "8a4dba9";
+      hash = "sha256-VIFQykbpa0PqdZUgm9Y+r5Xt9z6h0Tzigq1hPckKwmc=";
+    });
+  };
+
+  # see `nurl` to get the fetchfromgithub commands
 }
