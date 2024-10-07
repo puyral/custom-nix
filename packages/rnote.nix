80a8ce69f5eb52e36b55e1dafd0f7091a9298291
Puyral (mydos) <7871851+puyral@users.noreply.github.com>
2024-10-07 19:40:37 +0200

    nix fmt

--- packages/rnote.nix
+++ packages/rnote.nix
@@ -2,12 +2,16 @@
   packages = rec {
     rnote = pkgs.rnote.overrideAttrs (finalAttrs: previousAttrs: rec {
       src = rnote-src;
-      cargoDeps =
-        pkgs.rustPlatform.importCargoLock { lockFile = "${src}/Cargo.lock";outputHashes = {
-      "ink-stroke-modeler-rs-0.1.0" = "sha256-B6lT6qSOIHxqBpKTE4nO2+Xs9KF7JLVRUHOkYp8Sl+M=";
-      "piet-0.6.2" = "sha256-o1/MDvE0eHUaebkWbvQ0JRytMjvyvWLRmc2QtEeb+2I=";
-      "points_on_curve-0.5.0" = "sha256-1mmxG8oDdQ3KQtvIPT4TPgz+akdFah0uH6b1oJ4ASD0=";
-    }; };
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        lockFile = "${src}/Cargo.lock";
+        outputHashes = {
+          "ink-stroke-modeler-rs-0.1.0" =
+            "sha256-B6lT6qSOIHxqBpKTE4nO2+Xs9KF7JLVRUHOkYp8Sl+M=";
+          "piet-0.6.2" = "sha256-o1/MDvE0eHUaebkWbvQ0JRytMjvyvWLRmc2QtEeb+2I=";
+          "points_on_curve-0.5.0" =
+            "sha256-1mmxG8oDdQ3KQtvIPT4TPgz+akdFah0uH6b1oJ4ASD0=";
+        };
+      };
     });
   };
 }
