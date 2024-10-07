face6e0927af7e87aaf56a4e6e4eb2aef585fefb
puyral <7871851+puyral@users.noreply.github.com>
2024-10-07 16:04:26 +0200

    hash

--- packages/rnote.nix
+++ packages/rnote.nix
@@ -3,7 +3,10 @@
     rnote = pkgs.rnote.overrideAttrs (finalAttrs: previousAttrs: rec {
       src = rnote-src;
       cargoDeps =
-        pkgs.rustPlatform.importCargoLock { lockFile = "${src}/Cargo.lock"; };
+        pkgs.rustPlatform.importCargoLock { lockFile = "${src}/Cargo.lock";outputHashes = {
+      "ink-stroke-modeler-rs-0.1.0" = "";
+      "piet-0.6.2" = "";
+    }; };
     });
   };
 }
