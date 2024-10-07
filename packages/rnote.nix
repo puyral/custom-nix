85270edf4fa6a15fd634f90a3f3c40f802dcac4a
puyral <7871851+puyral@users.noreply.github.com>
2024-10-07 16:02:09 +0200

    fix ?

--- packages/rnote.nix
+++ packages/rnote.nix
@@ -1,6 +1,9 @@
 { rnote-src, pkgs, ... }: {
   packages = rec {
-    rnote = pkgs.rnote.overrideAttrs
-      (finalAttrs: previousAttrs: { src = rnote-src; });
+    rnote = pkgs.rnote.overrideAttrs (finalAttrs: previousAttrs: rec {
+      src = rnote-src;
+      cargoDeps =
+        pkgs.rustPlatform.importCargoLock { lockFile = "${src}/Cargo.lock"; };
+    });
   };
 }
