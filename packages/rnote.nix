6637a765a4e7c7a05a6a567551f11dd0d3149011
Puyral (mydos) <7871851+puyral@users.noreply.github.com>
2024-10-07 19:39:59 +0200

    rnote

--- packages/rnote.nix
+++ packages/rnote.nix
@@ -4,8 +4,9 @@
       src = rnote-src;
       cargoDeps =
         pkgs.rustPlatform.importCargoLock { lockFile = "${src}/Cargo.lock";outputHashes = {
-      "ink-stroke-modeler-rs-0.1.0" = "";
-      "piet-0.6.2" = "";
+      "ink-stroke-modeler-rs-0.1.0" = "sha256-B6lT6qSOIHxqBpKTE4nO2+Xs9KF7JLVRUHOkYp8Sl+M=";
+      "piet-0.6.2" = "sha256-o1/MDvE0eHUaebkWbvQ0JRytMjvyvWLRmc2QtEeb+2I=";
+      "points_on_curve-0.5.0" = "sha256-1mmxG8oDdQ3KQtvIPT4TPgz+akdFah0uH6b1oJ4ASD0=";
     }; };
     });
   };
