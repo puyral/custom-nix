9f2b822c0b58260e0a0b9c060ee5c20a8a7c0b4d
puyral <7871851+puyral@users.noreply.github.com>
2024-12-21 11:14:25 +0100

    set vampire to v4.9

--- packages/vampire.nix
+++ packages/vampire.nix
@@ -10,7 +10,27 @@ let
 
 in {
   packages = rec {
-    vampire = vampire-master;
+    vampire = pkgs.vampire.overrideAttrs (finalAttrs: previousAttrs: {
+      version = "4.9casc2024";
+      doCheck = true;
+      src = fetchFromGitHub {
+        owner = "vprover";
+        repo = "vampire";
+        rev = "v4.9casc2024";
+        hash = "sha256-NHAlPIy33u+TRmTuFoLRlPCvi3g62ilTfJ0wleboMNU=";
+      };
+      z3 = pkgs.z3.overrideAttrs (finalAttrs: previousAttrs: {
+        src = fetchFromGitHub {
+          owner = "Z3Prover";
+          repo = "z3";
+          rev = "79bbbf76d0c123481c8ca05cd3a98939270074d3";
+          sha256 = "sha256-5npD0gxGDHkGdB54Oqgt7Fpd1vQsaaNUNOuLAAcLbrg=";
+        };
+        version = "4.12.3-vampire";
+        # doCheck = false;
+      });
+      patches = [ ];
+    });
 
     vampire-master = mkvampire "master" vampire-master-src;
 
